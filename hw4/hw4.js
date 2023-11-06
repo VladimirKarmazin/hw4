87c4ce97a2a6dde7607a51ffcb391b3e6ebe0117
VladimirKarmazin <128676499+VladimirKarmazin@users.noreply.github.com>
2023-11-06 13:42:42 +0200

    Add files via upload

---
+++ hw4/hw4.js
@@ -0,0 +1,16 @@
+let number1 = parseFloat(prompt("Введіть перше число:"));
+let number2 = parseFloat(prompt("Введіть друге число:"));
+
+
+    let sum = number1 + number2;
+    alert("Результат додавання: " + sum);
+
+    let difference = number1 - number2;
+    alert("Результат віднімання: " + difference);
+
+    let product = number1 * number2;
+    alert("Результат множення: " + product);
+
+    let quotient = number1 / number2;
+    alert("Результат ділення: " + quotient);
+
